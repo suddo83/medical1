1301624f6ce2339c345eae13169f77e541a667a8
suddo83 <forevervocalists@gmail.com>
2024-03-18 16:13:45 +0900

    파이썬 수업내용

---
+++ P0318/P0318_09.py
@@ -0,0 +1,25 @@
+class Lotto:
+    number = 0
+    shape = 'circle'
+    
+    def __init__(self,number):
+        self.number = number
+        
+
+# lotto 1-45까지의 숫자를 입력한 리스트를 생성한 후, 출력하시오.
+
+l_list = []
+<<<<<<< HEAD
+for i in range(45):
+    l_list.append(Lotto(i+1))
+    print(l_list[i].number)
+
+# l1 = Lotto(1)
+# l2 = Lotto(2)
+# l3 = Lotto(3)
+=======
+
+l1 = Lotto(1)
+l2 = Lotto(2)
+l3 = Lotto(3)
+>>>>>>> 1f9b7528bc4cd9d2c868cdc59f682565e709bfc3
