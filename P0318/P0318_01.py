fb7402bebee56af68d5f49e4140d57da7addd7e3
suddo83 <forevervocalists@gmail.com>
2024-03-18 10:16:44 +0900

    파이썬 수업

--- P0318/P0318_01.py
+++ P0318/P0318_01.py
@@ -7,30 +7,18 @@ class Car:
     length = 4710
     width = 1800
     displace = 1600
+    speed = 0
+    
+    def upSpeed(self,sp):
+        self.speed += sp
+        
+    def downSpeed(self,sp):
+        self.speed -= sp
+        
+        
+        
+# 객체선언을 할때마다 제품(car)이 한개씩 생산
 
-def func():
-    pass
+c1 = Car() # 객체선언
 
-c1 = Car() # 클래스 객체선언 : Car클래스에 있는 모든 변수를 사용함
-print(c1.length)
 c2 = Car()
-c3 = Car()
-
-a = 1
-a1 = 1
-a2 = 1
-a3 = 1
-b = [1,2,3]
-
-a_l1 = 4710
-a_w1 = 1800
-a_d1 = 1600
-
-a_l2 = 4710
-a_w2 = 1800
-a_d2 = 1600
-
-a_l3 = 4710
-a_w3 = 1800
-a_d3 = 1600
-
